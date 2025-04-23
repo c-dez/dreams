3ffb4e2933fcbf61eac88239dadcb7a6d9b79403
c-dez <hdezcruzcarlos@gmail.com>
2025-04-23 13:20:55 -0600

    added Attack()

--- actors/player/Actions.cs
+++ actors/player/Actions.cs
@@ -3,19 +3,25 @@ using System;
 
 public partial class Actions : Node
 {
+	private AnimationNodeOneShot attackOneShot;
 	public override void _Ready()
 	{
+		// attackOneShot =(AnimationNodeOneShot) GetNode<AnimationNodeOneShot>("../AnimationTree").Get("parameters/AttackOneShot/request");
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
+		Attack();
 	}
 
 	private void Attack()
 	{
 		if (Input.IsActionJustPressed("mb1"))
 		{
-			GD.Print("mn1");
+			GD.Print("mb1");
+			GetNode<AnimationTree>("../AnimationTree").Set("parameters/AttackOneShot/request",  (int)AnimationNodeOneShot.OneShotRequest.Fire);
+
+
 		}
 	}
 }
