693c38818eb77de5e8936b24bbe8b935786a4a8f
c-dez <hdezcruzcarlos@gmail.com>
2025-04-23 13:48:38 -0600

    added AttackingToggle(bool)

--- actors/player/Actions.cs
+++ actors/player/Actions.cs
@@ -3,25 +3,29 @@ using System;
 
 public partial class Actions : Node
 {
-	private AnimationNodeOneShot attackOneShot;
-	public override void _Ready()
-	{
-		// attackOneShot =(AnimationNodeOneShot) GetNode<AnimationNodeOneShot>("../AnimationTree").Get("parameters/AttackOneShot/request");
-	}
+    private AnimationTree animationTree;
+    private bool isAttacking = false;
+    public override void _Ready()
+    {
+        animationTree = GetNode<AnimationTree>("../AnimationTree");
+    }
 
-	public override void _PhysicsProcess(double delta)
-	{
-		Attack();
-	}
+    public override void _PhysicsProcess(double delta)
+    {
+        Attack();
+    }
 
-	private void Attack()
-	{
-		if (Input.IsActionJustPressed("mb1"))
-		{
-			GD.Print("mb1");
-			GetNode<AnimationTree>("../AnimationTree").Set("parameters/AttackOneShot/request",  (int)AnimationNodeOneShot.OneShotRequest.Fire);
+    private void Attack()
+    {
+        if (Input.IsActionJustPressed("mb1") && !isAttacking)
+        {
+            animationTree.Set("parameters/AttackOneShot/request", (int)AnimationNodeOneShot.OneShotRequest.Fire);
+        }
+    }
 
 
-		}
-	}
+    private void AttackingToggle(bool value)
+    {
+        isAttacking = value;
+    }
 }
