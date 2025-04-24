33201e57f6164ec1a66da557052c8ca1c2369a3b
c-dez <hdezcruzcarlos@gmail.com>
2025-04-23 20:58:50 -0600

    anadido dos ataques en secuencia

--- actors/player/Actions.cs
+++ actors/player/Actions.cs
@@ -3,16 +3,22 @@ using System;
 
 public partial class Actions : Node
 {
+    [Export] Godot.Timer secondAttackTimer;
     private AnimationTree animationTree;
+    private AnimationNodeStateMachinePlayback attackStateMachine;
     private bool isAttacking = false;
     public override void _Ready()
     {
         animationTree = GetNode<AnimationTree>("../AnimationTree");
+        attackStateMachine = (AnimationNodeStateMachinePlayback)animationTree.Get("parameters/AttackStateMachine/playback");
+	
     }
 
     public override void _PhysicsProcess(double delta)
     {
         Attack();
+        GD.Print(isAttacking);
+        
     }
 
     private void Attack()
@@ -20,6 +26,7 @@ public partial class Actions : Node
         if (Input.IsActionJustPressed("mb1") && !isAttacking)
         {
             animationTree.Set("parameters/AttackOneShot/request", (int)AnimationNodeOneShot.OneShotRequest.Fire);
+            attackStateMachine.Travel(secondAttackTimer.TimeLeft > 0f ? "attack2" : "attack1");
         }
     }
 
@@ -27,5 +34,11 @@ public partial class Actions : Node
     private void AttackingToggle(bool value)
     {
         isAttacking = value;
+    }
+
+
+    private void StartSecondAttackTimer()
+    {
+        secondAttackTimer.Start();
     }
 }
